63763068a65c13ec639b6428ebbfd55fdf737241
He-He <lg3dx6fd@gmail.com>
2021-09-29 04:27:26 +0300

    Add project files.

---
+++ laba 1/RandomHardware.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "IRandomGenerator.h"
+#include <typeinfo>
+#include <random>
+#include <cstdint>
+namespace myr {
+    class RandomHardware :
+        public IRandomGenerator
+    {
+    public:
+        inline RandomHardware() : rd_(nullptr) {}
+        inline RandomHardware(std::random_device * rd) : rd_(rd) {};
+        inline ~RandomHardware() {
+            if (rd_) {
+                delete rd_;
+            }
+        }
+        inline virtual double operator() () const {
+            if (rd_) {
+                return (static_cast<double>((*rd_)()));
+            }
+            else {
+                return (static_cast<double>((std::random_device())()));
+            }
+        }
+        inline virtual double const max() const {
+            return max_;
+        }
+        inline virtual double const min() const {
+            return min_;
+        }
+        inline bool operator==(RandomHardware const& rval) const {
+            return 0;
+        }
+        inline bool operator!=(RandomHardware const& rval) const {
+            return 1;
+        }
+        inline bool operator==(IRandomGenerator const& rval) const {
+            if (typeid(*this) == typeid(rval)) {
+                return (*this) == (dynamic_cast<RandomHardware const&>(rval));;
+            }
+            else {
+                return false;
+            }
+        }
+        inline bool operator!= (IRandomGenerator const& rval) const {
+            return !((*this) == rval);
+        }
+    private:
+        static double const max_;
+        static double const min_;
+        std::random_device* rd_;
+
+        //restricted:
+        RandomHardware& operator= (RandomHardware const&) = delete;
+
+    };
+
+    double const RandomHardware::max_ = std::random_device::max();
+    double const RandomHardware::min_ = std::random_device::min();
+}
